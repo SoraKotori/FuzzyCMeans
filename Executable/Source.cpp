18305e5ee607aec13e0c8fd579d94f8b10db773e
Kotori Sora <b1035151@nuk.edu.tw>
2017-04-06 00:50:08 +0800

    Distance

--- Executable/Source.cpp
+++ Executable/Source.cpp
@@ -8,6 +8,7 @@
 #include <fstream>
 #include <sstream>
 #include <cstring>
+#include <utility>
 
 #include <iostream>
 #include <filesystem>
@@ -15,285 +16,302 @@
 using namespace std;
 using namespace std::experimental::filesystem;
 
-#define ICAL
+template<typename _FileName, typename _Ty, typename _Size>
+bool csv_read(_FileName&& __filename, vector<_Ty>& __data, _Size& __dimension, _Size& __data_count);
+
+template<typename _FileName, typename _Ty, typename _Size>
+bool csv_write(const _FileName& __filename, const vector<_Ty>& _Center, const vector<_Ty>& _Data, const vector<_Size>& _DataCluster);
+
+
+template<typename _InputIterator1, typename _InputIterator2,
+	typename _Type = typename _InputIterator1::value_type>
+	inline auto SumSquares(_InputIterator1 __first1, _InputIterator1 __last1,
+		_InputIterator2 __first2, _Type __init = _Type(0))
+{
+	for (; __first1 != __last1; ++__first1, ++__first2)
+	{
+		auto __minus = *__first1 - *__first2;
+		__init += __minus * __minus;
+	}
+
+	return __init;
+}
+
+template<typename... _Args>
+inline auto EuclideanDistance(_Args&&... __args)
+{
+	return sqrt(SumSquares(forward<_Args>(__args)...));
+}
+
+template<typename _Ty, typename _Size>
+auto csvDistance(const vector<_Ty>& _Center, const vector<_Ty>& _Data, const vector<_Size>& _DataCluster)
+{
+	decltype(auto) _DataCount = _DataCluster.size();
+	decltype(auto) _Dimension = (_Data.size() / _DataCount);
+	decltype(auto) _CenterCount = (_Center.size() / _Dimension);
+
+	vector<_Size> _Count(_CenterCount, 0);
+	vector<_Ty> _Distance(_CenterCount, 0);
+
+	for (decltype(_DataCount) _DataIndex = 0; _DataIndex < _DataCount; ++_DataIndex)
+	{
+		decltype(auto) _DataClusterIndex = _DataCluster[_DataIndex];
+		decltype(auto) _DataIndexFirst = &_Data[_DataIndex];
+		decltype(auto) _CenterIndexFirst = &_Center[_DataClusterIndex];
+
+		_Count[_DataClusterIndex]++;
+		_Distance[_DataClusterIndex] +=
+			EuclideanDistance(_DataIndexFirst, _DataIndexFirst + _Dimension, _CenterIndexFirst, _Ty(0));
+	}
+
+	decltype(auto) _First = _Count.begin();
+	decltype(auto) _Last = _Count.end();
+	decltype(auto) _Result = _Distance.begin();
+
+	for (; _First != _Last; ++_First, ++_Result)
+	{
+		*_Result /= *_First;
+	}
+
+	path _Directories(to_string(_CenterCount));
+	ofstream _FileDistance(_Directories / "Distance.csv");
+
+	_FileDistance << "id" << endl;
+	for (decltype(_CenterCount) _CenterIndex = 0; _CenterIndex < _CenterCount; _CenterIndex++)
+	{
+		_FileDistance << _CenterIndex + 1;
+		_FileDistance << ',' << _Distance[_CenterIndex];
+		_FileDistance << endl;;
+	}
+
+	return move(_Distance);
+}
+int main(int argc, char *argv[])
+{
+	if (6 != argc)
+	{
+		return EXIT_FAILURE;
+	}
+
+	auto _FileName = string(argv[1]);
+	auto _OutputFileName = string(argv[2]);
+	auto _kCluster = atoi(argv[3]);
+	auto _mWeight = strtod(argv[4], nullptr);
+	auto _Epsilon = strtod(argv[5], nullptr);
+
+	srand((unsigned)time(NULL));
+	vector<double> _Data;
+	int _Dimension;
+	int _DataSize;
+
+	bool _bResult = csv_read(_FileName, _Data, _Dimension, _DataSize);
+	if (false == _bResult)
+	{
+		return EXIT_FAILURE;
+	}
+
+	FuzzyCMeans _FCM;
+	_bResult = _FCM.Create(_Data.data(), _Dimension, _DataSize, _kCluster, _mWeight, _Epsilon, FuzzyCMeans::EnumInitializeMethod::OriginData);
+	if (false == _bResult)
+	{
+		return EXIT_FAILURE;
+	}
+
+	int Iteration;
+	for (Iteration = 0; Iteration < 100000 && _FCM.Run(); Iteration++);
+
+	vector<int> _DataCluster(_DataSize);
+	for (int Index = 0; Index < _DataSize; _DataCluster[Index] = _FCM.GetDataCluster(Index), Index++);
+
+	auto _Center = [&, _pCenter = _FCM.GetCenter()]
+	{ return vector<double>(_pCenter, _pCenter + _kCluster * _Dimension); }();
+
+	_bResult = csv_write(_OutputFileName, _Center, _Data, _DataCluster);
+	if (false == _bResult)
+	{
+		return EXIT_FAILURE;
+	}
+
+	decltype(auto) _Distance = csvDistance(_Center, _Data, _DataCluster);
+	copy(_Distance.begin(), _Distance.end(), ostream_iterator<double>(cout, "\n"));
+
+
+	return EXIT_SUCCESS;
+}
 
 class csv_ifstream : virtual public ifstream
 {
 public:
-    template<typename... _Args>
-    explicit csv_ifstream(_Args&&... __args) : ifstream(forward<_Args>(__args)...)
-    {
-        struct csv_type : ctype<char>
-        {
-            static const mask* get_table()
-            {
-                mask* __csv_table = new mask[table_size];
-                memcpy(__csv_table, classic_table(), sizeof(mask) * table_size);
+	template<typename... _Args>
+	explicit csv_ifstream(_Args&&... __args) : ifstream(forward<_Args>(__args)...)
+	{
+		struct csv_type : ctype<char>
+		{
+			static const mask* get_table()
+			{
+				mask* __csv_table = new mask[table_size];
+				memcpy(__csv_table, classic_table(), sizeof(mask) * table_size);
 
-                __csv_table[','] |= space;
-                return __csv_table;
-            }
+				__csv_table[','] |= space;
+				return __csv_table;
+			}
 
-            csv_type(size_t __refs = 0U) : ctype(get_table(), true, __refs)
-            { }
-        };
+			csv_type(size_t __refs = 0U) : ctype(get_table(), true, __refs)
+			{ }
+		};
 
-        imbue(locale(getloc(), new csv_type));
-    }
+		imbue(locale(getloc(), new csv_type));
+	}
 };
 
 template<typename _FileName, typename _Ty, typename _Size>
 bool csv_read(_FileName&& __filename, vector<_Ty>& __data, _Size& __dimension, _Size& __data_count)
 {
-    typedef typename vector<_Ty>::size_type size_type;
+	typedef typename vector<_Ty>::size_type size_type;
 
-    csv_ifstream __file(forward<_FileName>(__filename));
-    if (!__file)
-    {
-        return false;
-    }
+	csv_ifstream __file(forward<_FileName>(__filename));
+	if (!__file)
+	{
+		return false;
+	}
 
-    size_type __comma = 0;
-    size_type __line = 0;
+	size_type __comma = 0;
+	size_type __line = 0;
 
-    for (auto __char = __file.get(); EOF != __char; __char = __file.get())
-    {
-        switch (__char)
-        {
-        case ',':
-            __comma++;
-            break;
-        case '\n':
-            __line++;
-            break;
-        }
-    }
+	for (auto __char = __file.get(); EOF != __char; __char = __file.get())
+	{
+		switch (__char)
+		{
+		case ',':
+			__comma++;
+			break;
+		case '\n':
+			__line++;
+			break;
+		}
+	}
 
-    if (0 == __comma || 0 == __line)
-    {
-        return false;
-    }
+	if (0 == __comma || 0 == __line)
+	{
+		return false;
+	}
 
-    __file.clear();
-    __file.seekg(0, __file.beg);
+	__file.clear();
+	__file.seekg(0, __file.beg);
 
-#ifdef ICAL
-    string _FirstLine;
-    getline(__file, _FirstLine);
-#endif // ICAL
+	string _FirstLine;
+	getline(__file, _FirstLine);
 
-    size_type __fdimension = __comma / __line + 1;
-    size_type __fdata_count = __line;
+	size_type __fdimension = __comma / __line;
+	size_type __fdata_count = __line - 1;
+	vector<_Ty> __fdata(__fdimension * __fdata_count);
 
-#ifdef ICAL
-    __fdimension -= 1;
-    __fdata_count -= 1;
-#endif // ICAL
+	auto __it_fdata = __fdata.begin();
+	for (size_type __fdata_index = 0; __fdata_index < __fdata_count; __fdata_index++)
+	{
+		_Ty __data_id;
+		__file >> __data_id;
 
-    vector<_Ty> __fdata(__fdimension * __fdata_count);
+		__it_fdata = copy_n(istream_iterator<_Ty>(__file), __fdimension, __it_fdata);
+	}
 
-#ifdef ICAL
-    auto __it_fdata = __fdata.begin();
-    for (size_type __fdata_index = 0; __fdata_index < __fdata_count; __fdata_index++)
-    {
-        _Ty __data_id;
-        __file >> __data_id;
+	__data = move(__fdata);
+	__dimension = static_cast<_Size>(__fdimension);
+	__data_count = static_cast<_Size>(__fdata_count);
 
-        __it_fdata = copy_n(istream_iterator<_Ty>(__file), __fdimension, __it_fdata);
-    }
-#else
-    copy_n(istream_iterator<_Ty>(__file), __fdata.size(), __fdata.begin());
-#endif // ICAL
-
-    __data = move(__fdata);
-    __dimension = static_cast<_Size>(__fdimension);
-    __data_count = static_cast<_Size>(__fdata_count);
-
-    return true;
+	return true;
 }
 
 template<typename _FileName, typename _Ty, typename _Size>
-bool csv_write(const _FileName& __filename, const vector<_Ty>& __center, const vector<_Ty>& __data, const vector<_Size>& __data_cluster, _Size __cluster)
+bool csv_write(const _FileName& __filename, const vector<_Ty>& _Center, const vector<_Ty>& _Data, const vector<_Size>& _DataCluster)
 {
-    typedef typename vector<_Ty>::size_type size_type;
-    size_type __line = static_cast<size_type>(__data_cluster.size());
-    size_type __dimension = __data.size() / __line;
+	typedef typename vector<_Ty>::size_type size_type;
+	auto _DataCount = static_cast<size_type>(_DataCluster.size());
+	auto _Dimension = _Data.size() / _DataCount;
+	auto _ClusterCount = _Center.size() / _Dimension;
 
-    ostringstream __cluster_string;
-    __cluster_string << __cluster;
-    path __file_directories(__cluster_string.str());
+	path _Directories(to_string(_ClusterCount));
+	bool bResult = is_directory(_Directories);
+	if (!bResult)
+	{
+		bResult = create_directories(_Directories);
+		if (!bResult)
+		{
+			return false;
+		}
+	}
 
-    bool bResult = is_directory(__file_directories);
-    if (!bResult)
-    {
-        bResult = create_directories(__file_directories);
-        if (!bResult)
-        {
-            return false;
-        }
-    }
+	ofstream _FileCenter(_Directories / "Center.csv");
+	if (!_FileCenter)
+	{
+		return false;
+	}
 
-    path __file_path = __file_directories / "Center.csv";
-    ofstream __file_center(__file_path);
-    if (!__file_center)
-    {
-        return false;
-    }
+	ofstream _FileOutput(_Directories / __filename);
+	if (!_FileOutput)
+	{
+		return false;
+	}
 
-    __file_path = __file_directories / __filename;
-    ofstream __file(__file_path);
-    if (!__file)
-    {
-        return false;
-    }
+	vector<ofstream> _FileCluster(_ClusterCount);
+	for (decltype(_ClusterCount) _ClusterIndex = 0; _ClusterIndex < _ClusterCount; _ClusterIndex++)
+	{
+		ofstream _FileClusterIndex(_Directories / (to_string(_ClusterIndex) + ".csv"));
+		if (!_FileClusterIndex)
+		{
+			return false;
+		}
 
-    vector<ofstream> __file_cluster(__cluster);
-    for (_Size __cluster_index = 0; __cluster_index < __cluster; __cluster_index++)
-    {
-        __cluster_string.seekp(0, ios::beg);
-        __cluster_string << __cluster_index << ".csv";
-        __file_path = __file_directories / __cluster_string.str();
+		_FileCluster[_ClusterIndex] = move(_FileClusterIndex);
+	}
 
-        __file_cluster[__cluster_index] = ofstream(__file_path);
-        if (!__file_cluster[__cluster_index])
-        {
-            return false;
-        }
-    }
+	_FileCenter << "id";
+	_FileOutput << "id";
+	for (size_type _DimensionIndex = 0; _DimensionIndex < _Dimension; _DimensionIndex++)
+	{
+		auto _FirstLine = string(",dim") + to_string(_DimensionIndex + 1);
 
-    __file_center << "id";
-    __file << "id";
-    for (size_type __dimension_index = 0; __dimension_index < __dimension; __dimension_index++)
-    {
-        auto __dimension_index_add = __dimension_index + 1;
+		_FileCenter << _FirstLine;
+		_FileOutput << _FirstLine;
+	}
+	_FileCenter << endl;
+	_FileOutput << ',' << "cid" << endl;;
 
-        __file_center << ',' << "dim" << __dimension_index_add;
-        __file << ',' << "dim" << __dimension_index_add;
-    }
-    __file_center << endl;;
-    __file << ',' << "cid" << endl;;
+	auto _CenterFirst = _Center.begin();
+	for (decltype(_ClusterCount) _ClusterIndex = 0; _ClusterIndex < _ClusterCount; _ClusterIndex++)
+	{
+		decltype(auto) _FileClusterIndex = _FileCluster[_ClusterIndex];
 
-    auto __center_begin = __center.begin();
-    for (_Size __cluster_index = 0; __cluster_index < __cluster; __cluster_index++)
-    {
-        auto& __file_cluster_index = __file_cluster[__cluster_index];
-        
-        __file_center << __cluster_index + 1;
-        __file_cluster_index << "id";
-        for (size_type __dimension_index = 0; __dimension_index < __dimension; __dimension_index++)
-        {
-            __file_center << ',' << *__center_begin++;
-            __file_cluster_index << ',' << "dim" << __dimension_index + 1;
-        }
-        __file_center << endl;;
-        __file_cluster_index << ',' << "cid" << endl;;
-    }
+		_FileCenter << _ClusterIndex + 1;
+		_FileClusterIndex << "id";
+		for (size_type _DimensionIndex = 0; _DimensionIndex < _Dimension; _DimensionIndex++)
+		{
+			_FileCenter << ',' << *_CenterFirst++;
+			_FileClusterIndex << ',' << "dim" << _DimensionIndex + 1;
+		}
+		_FileCenter << endl;;
+		_FileClusterIndex << ',' << "cid" << endl;;
+	}
 
-    auto __data_begin = __data.begin();
-    for (size_type __data_index = 0; __data_index < __line; __data_index++)
-    {
-        auto __cluster_index = __data_cluster[__data_index];
-        auto& __file_cluster_index = __file_cluster[__cluster_index];
+	auto _DataFirst = _Data.begin();
+	for (size_type _DataIndex = 0; _DataIndex < _DataCount; _DataIndex++)
+	{
+		decltype(auto) _DataClusterIndex = _DataCluster[_DataIndex];
+		decltype(auto) _FileClusterIndex = _FileCluster[_DataClusterIndex];
 
-        auto __data_index_add = __data_index + 1;
-        __file << __data_index_add;
-        __file_cluster_index << __data_index_add;
+		auto _DataIndexAdd = _DataIndex + 1;
+		_FileOutput << _DataIndexAdd;
+		_FileClusterIndex << _DataIndexAdd;
+		for (size_type _DimensionIndex = 0; _DimensionIndex < _Dimension; _DimensionIndex++)
+		{
+			auto _DataValue = *_DataFirst++;
 
-        for (size_type __dimension_index = 0; __dimension_index < __dimension; __dimension_index++)
-        {
-            auto __data_value = *__data_begin++;
+			_FileOutput << ',' << _DataValue;
+			_FileClusterIndex << ',' << _DataValue;
+		}
+		_FileOutput << ',' << _DataClusterIndex << endl;
+		_FileClusterIndex << ',' << _DataClusterIndex << endl;
+	}
 
-            __file << ',' << __data_value;
-            __file_cluster_index << ',' << __data_value;
-        }
-        __file << ',' << __cluster_index << endl;
-        __file_cluster_index << ',' << __cluster_index << endl;
-    }
-
-    return true;
-}
-
-int main(int argc, char *argv[])
-{
-    if (6 != argc)
-    {
-        return EXIT_FAILURE;
-    }
-
-    string _FileName = string(argv[1]);
-    string _OutputFileName = string(argv[2]);
-
-    int _kCluster;
-    istringstream(string(argv[3])) >> _kCluster;
-
-    double _mWeight;
-    istringstream(string(argv[4])) >> _mWeight;
-
-    double _Epsilon;
-    istringstream(string(argv[5])) >> _Epsilon;
-
-    vector<double> _Data;
-    int _Dimension;
-    int _DataSize;
-
-    bool _bResult = csv_read(_FileName, _Data, _Dimension, _DataSize);
-    if (false == _bResult)
-    {
-        cout << ".csv Read Failed" << endl;
-        return EXIT_FAILURE;
-    }
-
-    srand(time(NULL));
-    FuzzyCMeans _FCM;
-    _bResult = _FCM.Create(_Data.data(), _Dimension, _DataSize, _kCluster, _mWeight, _Epsilon, FuzzyCMeans::EnumInitializeMethod::OriginData);
-    if (false == _bResult)
-    {
-        cout << "Create Failed" << endl;
-        return EXIT_FAILURE;
-    }
-
-    int Iteration;
-    for (Iteration = 0; Iteration < 100000; Iteration++)
-    {
-        bool Run = _FCM.Run();
-
-#ifndef ICAL
-        cout << "Iteration : " << Iteration << endl;
-        for (int DataIndex = 0; DataIndex < _DataSize; DataIndex++)
-        {
-            cout << "Data[" << DataIndex << "] " << _FCM.GetDataCluster(DataIndex) << " Cluster" << endl;
-        }
-        cout << endl;
-#endif // !ICAL
-
-        if (false == Run)
-        {
-            Iteration++;
-            break;
-        }
-    }
-
-    vector<int> _DataCluster(_DataSize);
-
-    cout << "IterationCount : " << Iteration << endl;
-    for (int _DataIndex = 0; _DataIndex < _DataSize; _DataIndex++)
-    {
-        auto _Cluster = _FCM.GetDataCluster(_DataIndex);
-        _DataCluster[_DataIndex] = _Cluster;
-
-        cout << "Data[" << _DataIndex << "] " << _Cluster << " Cluster" << endl;
-    }
-    cout << endl;
-
-    double* _pCenter = _FCM.GetCenter();
-    vector<double> _Center(_pCenter, _pCenter + sizeof(double) * _kCluster * _Dimension);
-
-    _bResult = csv_write(_OutputFileName, _Center, _Data, _DataCluster, _kCluster);
-    if (false == _bResult)
-    {
-        cout << "Create Failed" << endl;
-        return EXIT_FAILURE;
-    }
-
-    return EXIT_SUCCESS;
+	return true;
 }
